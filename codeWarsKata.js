23245835e18e8886f1f3da1f15c2895c84c102bb
DidierShemaGatete <didiershemagate01@gmail.com>
2023-06-02 19:42:21 +0300

    Multiples of 3 or 5

--- codeWarsKata.js
+++ codeWarsKata.js
@@ -9,3 +9,28 @@ function getMichaelLastName(inputText) {
     return lastNames;
   }
   
+
+  /* ----------------------------------------------------------------------- */
+
+
+/* Extract names of people named michael: */
+
+function solution(number) {
+    if (number < 0) {
+      return 0;
+    }
+  
+    let sum = 0;
+    for (let i = 1; i < number; i++) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        sum += i;
+      }
+    }
+  
+    return sum;
+  }
+
+  
+
+  /* ------------------------------------------------ */
+
